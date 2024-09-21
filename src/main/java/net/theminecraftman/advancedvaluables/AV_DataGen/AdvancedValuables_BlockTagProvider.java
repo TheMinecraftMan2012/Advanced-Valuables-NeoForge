0182b657baf953beeab9c632ebdc9a4fe354713c
TheMinecraftMan2012 <h.codingal@gmail.com>
2024-09-21 15:41:20 +0600

    Data Generator

--- src/main/java/net/theminecraftman/advancedvaluables/AV_DataGen/AdvancedValuables_BlockTagProvider.java
+++ src/main/java/net/theminecraftman/advancedvaluables/AV_DataGen/AdvancedValuables_BlockTagProvider.java
@@ -3,7 +3,6 @@ package net.theminecraftman.advancedvaluables.AV_DataGen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.theminecraftman.advancedvaluables.AV_Templates.AdvancedValuables_BlockClass;
